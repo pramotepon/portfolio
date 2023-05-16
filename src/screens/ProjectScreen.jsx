bdc777de207826b05ce27e69dd657b0854d1af40
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-05-16 21:46:51 +0700

    Deploy

--- src/screens/ProjectScreen.jsx
+++ src/screens/ProjectScreen.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ScreenLayout from '../layouts/ScreenLayout'
+import GitHubCalendar from 'react-github-calendar'
 
 const ProjectScreen = () => {
   return (
     <ScreenLayout title={'Projects'}>
-      <div>
-        <h1>Project</h1>
+      <div className='min-h-screen'>
+        <h2 className='text-tertiary text-6xl pt-44 text-center drop-shadow-title-shadow'>PROJECTS</h2>
+        <div className='text-secondary w-fit mx-auto mt-10'>
+          <GitHubCalendar username='pramotepon' colorScheme='dark' />
+        </div>
       </div>
     </ScreenLayout>
   )
