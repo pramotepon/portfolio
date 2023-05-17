7a44f526b8fe80ea27ae4d4211d673f9fc94d36e
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-05-17 10:47:06 +0700

    Responsive navbar and home page

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -5,10 +5,10 @@ const HomeScreen = () => {
   return (
     <ScreenLayout title={'Home'}>
       <div className='flex min-h-screen'>
-        <div className='my-auto'>
-          <h1 className='text-tertiary text-9xl'>I am</h1>
-          <h1 className='text-tertiary text-9xl'>Full Stack</h1>
-          <h1 className='text-tertiary text-9xl'>Web Developer</h1>
+        <div className='my-auto text-6xl sm:text-8xl md:text-9xl'>
+          <h1 className='text-tertiary'>I am</h1>
+          <h1 className='text-tertiary'>Full Stack</h1>
+          <h1 className='text-tertiary'>Web Developer</h1>
         </div>
       </div>
     </ScreenLayout>
