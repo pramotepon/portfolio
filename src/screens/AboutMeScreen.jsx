4c85ede9a0b606a7ec207a2f07ea567db5e5bfc7
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-05-25 14:59:26 +0700

    Add animation text

--- src/screens/AboutMeScreen.jsx
+++ src/screens/AboutMeScreen.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ScreenLayout from '../layouts/ScreenLayout'
 
+// Css
+import '../assets/css/screens/animation.css';
+
 const AboutMeScreen = () => {
   return (
     <ScreenLayout title={'About me'}>
@@ -11,14 +14,16 @@ const AboutMeScreen = () => {
           </div>
           <div className='flex-auto w-auto lg:w-64 lg:pl-20'>
             <div className='h-full my-auto'>
-              <h2 className='text-tertiary text-6xl mt-16 drop-shadow-title-shadow'>About me</h2>
+              <h1 className='text-6xl mt-16 neon-title-animation'>Pramote Phanon</h1>
+              <hr className='text-white' />
+              <h2 className='text-tertiary text-6xl drop-shadow-title-shadow'>About me</h2>
               <p className='lg:text-3xl text-xl sm:mt-16 mt-0 text-secondary'>
                 As a <span className='text-tertiary drop-shadow-title-shadow'>Growth-minded</span> Web Developer with 3+ years of experience in
                 <span className='text-tertiary drop-shadow-title-shadow'>PHP</span>, <span className='text-tertiary drop-shadow-title-shadow'>HTML</span>,
                 <span className='text-tertiary drop-shadow-title-shadow'>CSS</span>, and <span className='text-tertiary drop-shadow-title-shadow'>SQL</span>,
                 I am dedicated to <span className='text-tertiary drop-shadow-title-shadow'>continuously expanding my skills and knowledge</span>.
                 I recently graduated from JSD4 with expertise in <span className='text-tertiary drop-shadow-title-shadow'>MERN Stack</span> technology,
-                and I am excited to apply this knowledge to new projects. I am a team leader and team player with excellent 
+                and I am excited to apply this knowledge to new projects. I am a team leader and team player with excellent
                 <span className='text-tertiary drop-shadow-title-shadow'> communication</span> and <span className='text-tertiary drop-shadow-title-shadow'>collaboration</span> skills,
                 committed to delivering high-quality work that exceeds expectations.
               </p>
